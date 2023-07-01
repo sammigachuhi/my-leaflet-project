76233749dc6283ee9138bd224ad4e6e5b50547d0
Samuel Gachuhi <sammigachuhi@gmail.com>
2023-07-01 20:17:59 +0300

    Chapter 11 edited, summary

--- my-leaflet-vs/heatmap2.js
+++ my-leaflet-vs/heatmap2.js
@@ -7,6 +7,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 ///////////////////////////////////////////////////////
 var geoJsonUrl = "https://raw.githubusercontent.com/sammigachuhi/geojson_files/main/selected_hospitals.json";
 
+
 L.geoJson.ajax(geoJsonUrl, {
     //
 
@@ -14,26 +15,35 @@ L.geoJson.ajax(geoJsonUrl, {
       // Create a heatmap layer and add the GeoJSON data to it
       var heatmapLayer = L.heatLayer(geoJSON.features.map(function (feature) {
         return [feature.geometry.coordinates[1], feature.geometry.coordinates[0]];
-      }))
-    }
+      }))   
+    },
+
+    // heatmapLayer.addTo(map),
   }).addTo(map);
 
-heatmapLayer.setOptions({
-    // Customize heatmap options here, such as radius, gradient, etc.
-    radius: 40,
-    blur: 10, 
-    gradient: {
-            '0': 'Navy', '0.25': 'Navy',
-            '0.26': 'Green',
-            '0.5': 'Green',
-            '0.51': 'Yellow',
-            '0.75': 'Yellow',
-            '0.76': 'Red',
-            '1': 'Red'
-            }
-});
+heatmapLayer.addTo(map);
 
-heatmapLayer.addTo(map); // Add the heatmap layer to the map
+
+
+// heatmapLayer.setOptions({
+//     // Customize heatmap options here, such as radius, gradient, etc.
+//     radius: 40,
+//     blur: 10, 
+//     gradient: {
+//             '0': 'Navy', '0.25': 'Navy',
+//             '0.26': 'Green',
+//             '0.5': 'Green',
+//             '0.51': 'Yellow',
+//             '0.75': 'Yellow',
+//             '0.76': 'Red',
+//             '1': 'Red'
+//             }
+// });
+
+// heatmapLayer.addTo(map); // Add the heatmap layer to the map
   
+
+/////
+
 
 
